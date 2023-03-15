dc8ba6c7ee27b3a051dd37fe12575dfdaea4d6eb
Fernando Dias <56140193+nand0diaz@users.noreply.github.com>
2023-03-15 19:40:06 -0300

    Connect wallet button

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,5 @@
 import Head from "next/head"
+import { ConnectWallet } from "@thirdweb-dev/react"
 
 export default function Home() {
 	return (
@@ -9,7 +10,9 @@ export default function Home() {
 				<meta name="viewport" content="width=device-width, initial-scale=1" />
 				<link rel="icon" href="/favicon.ico" />
 			</Head>
-			<></>
+			<>
+				<ConnectWallet />
+			</>
 		</>
 	)
 }
